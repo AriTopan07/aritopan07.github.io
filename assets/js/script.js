dbcce35899d3f9ea13c857bbb9a61f01d934781c
AriTopan07 <aritopan67@gmail.com>
2023-07-04 23:18:42 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var orderButton1 = document.getElementById('orderButton1');
 var orderButton2 = document.getElementById('orderButton2');
 var orderButton3 = document.getElementById('orderButton3');
 
-var phoneNumber = '6281234567890'; // Ganti dengan nomor WhatsApp tujuan
+var phoneNumber = '6282141938309'; // Ganti dengan nomor WhatsApp tujuan
 var message = 'Halo, saya tertarik untuk memesan produk Anda'; // Ganti dengan pesan yang diinginkan
 
 orderButton1.addEventListener('click', function() {
