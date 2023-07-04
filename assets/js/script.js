945ac1c37b6ac7b8477efd4f41a815f5883f1b07
AriTopan07 <aritopan67@gmail.com>
2023-07-05 03:06:51 +0700

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
+var orderButton = document.getElementById('orderButton');
 var orderButton1 = document.getElementById('orderButton1');
 var orderButton2 = document.getElementById('orderButton2');
 var orderButton3 = document.getElementById('orderButton3');
 
-var phoneNumber = '6282141938309'; // Ganti dengan nomor WhatsApp tujuan
-var message = 'Halo, saya tertarik untuk memesan produk Anda'; // Ganti dengan pesan yang diinginkan
+var phoneNumber = '6282141938309';
+var message = 'Halo, saya tertarik untuk memesan produk Anda';
+
+orderButton.addEventListener('click', function() {
+    openWhatsAppChat(message);
+});
 
 orderButton1.addEventListener('click', function() {
     openWhatsAppChat(message);
