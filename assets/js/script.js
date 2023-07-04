e401bc712c4d750110156a884cbf34719eb4307a
AriTopan07 <aritopan67@gmail.com>
2023-07-04 23:14:19 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-let orderButton = document.getElementById('orderButton');
-let phoneNumber = '6281234567890'; // Ganti dengan nomor WhatsApp tujuan
-let message = 'Halo, saya tertarik untuk membeli produk Anda'; // Ganti dengan pesan yang diinginkan
+var orderButton1 = document.getElementById('orderButton1');
+var orderButton2 = document.getElementById('orderButton2');
+var orderButton3 = document.getElementById('orderButton3');
 
-orderButton.addEventListener('click', function() {
-  let encodedMessage = encodeURIComponent(message);
-  let whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodedMessage;
-  window.open(whatsappLink, '_blank');
+var phoneNumber = '6281234567890'; // Ganti dengan nomor WhatsApp tujuan
+var message = 'Halo, saya tertarik untuk memesan produk Anda'; // Ganti dengan pesan yang diinginkan
+
+orderButton1.addEventListener('click', function() {
+    openWhatsAppChat(message);
 });
+
+orderButton2.addEventListener('click', function() {
+    openWhatsAppChat(message);
+});
+
+orderButton3.addEventListener('click', function() {
+    openWhatsAppChat(message);
+});
+
+function openWhatsAppChat(message) {
+    var encodedMessage = encodeURIComponent(message);
+    var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodedMessage;
+    window.open(whatsappLink, '_blank');
+}
